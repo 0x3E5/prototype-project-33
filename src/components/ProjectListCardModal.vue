232f3b650792959dff5fb5d49e909c1077f2b5a5
wangyuequan <wangyuequan@sword-tech.com>
2021-09-22 16:06:19 +0800

    弹窗文件创建

---
+++ src/components/ProjectListCardModal.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: 0x3E5
+ * @Date: 2021-09-22 14:54:35
+ * @LastEditTime: 2021-09-22 16:05:30
+ * @LastEditors: 0x3E5
+ * @Description: 
+ * @FilePath: \prototype-project-33\src\components\ProjectListCardModal.vue
+-->
+<template>
+
+</template>
+
+<script>
+export default{
+  name: 'ProjectListCardModal',
+}
+</script>
+
+<style scoped>
+
+</style>
